c374431eb8dc6905418ce5e9561f6e70e693151a
Sviatoslav Andrushko <andsviat@gmail.com>
2018-11-09 11:59:10 +0200

    Update the webpack configuration in Angular Plugin (#19)

--- src/plugins/AngularPlugin.ts
+++ src/plugins/AngularPlugin.ts
@@ -1,5 +1,3 @@
-import * as path from 'path';
-
 import { Builder } from '../Builder';
 import { ConfigPlugin } from '../ConfigPlugin';
 import Spin from '../Spin';
@@ -20,16 +18,35 @@ export default class AngularPlugin implements ConfigPlugin {
             {
               test: tsRule.test,
               use: { loader: 'angular2-template-loader', options: spin.createConfig(builder, 'angular2Template', {}) }
+            },
+            /**
+             * Enabling the SystemJS parser for the .js files under @angular/core/ will disable
+             * the deprecation warnings.
+             * See https://prntscr.com/lezfmd
+             *
+             * SystemJS is disabled by default for webpack.
+             * See https://webpack.js.org/configuration/module/#rule-parser
+             *
+             * Note that the webpack.ContextReplacementPlugin must also be properly configured (see below).
+             */
+            {
+              test: /[\\\/]@angular[\\\/]core[\\\/].+\.js$/,
+              parser: { system: true }
             }
           ]
         },
         plugins: [
           // Workaround for angular/angular#11580
           new webpack.ContextReplacementPlugin(
-            // The (\\|\/) piece accounts for path separators in *nix and Windows
-            /angular[\\\/]core[\\\/]@angular/,
-            path.join(builder.require.cwd, 'src'),
-            {} // a map of your routes
+            /**
+             * Override the initial configuration for ContextReplacementPlugin.
+             * The argument /angular[\\\/]core[\\\/]@angular/ causes several warnings.
+             * See https://prnt.sc/lezevh
+             *
+             * It's necessary to remove the @angular part and fix the issue by enabling
+             * the proper module.rules.Rule for @angular/core/*.js files(see above).
+             */
+            /angular[\\\/]core/
           )
         ]
       });
